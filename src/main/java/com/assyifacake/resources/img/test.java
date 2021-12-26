8acd1ba478ae87615ef64e97b443900167b1e75c
ihsansfd <ihsannurul@hotmail.com>
2021-12-26 11:20:41 +0700

    kudu tambah file klo folder kosong gk ke push

---
+++ src/main/java/com/assyifacake/resources/img/test.java
@@ -0,0 +1,14 @@
+package com.assyifacake.resources.img;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class test {
+    
+}
